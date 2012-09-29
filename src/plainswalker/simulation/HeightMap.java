954a0f3781defba554bd575c51e3e92a4f535c1c
Bee Sharwood <bee.sharwood@gmail.com>
2012-09-29 13:54:29 +0200

    HeightMap now functional
    Saving before some signifant reworking of terrain data storing

--- src/plainswalker/simulation/HeightMap.java
+++ src/plainswalker/simulation/HeightMap.java
@@ -9,21 +9,19 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
+
 public class HeightMap{
 	// height grid: 2D array
 	double[][] heightgrid;
 	int length = 100, width = 100;
+	float xstep, ystep;
+	double lowest = Double.POSITIVE_INFINITY, highest = Double.NEGATIVE_INFINITY, range = 0;
 	
-	/*// FOR TESTING: REMOVE LATER
-	public static void main(String[] args) {
-		//HeightMap a = new HeightMap("GC2.map");
-		//System.out.println(a);
-		
-		HeightMap a = new HeightMap("test.map");
-		System.out.println("Interpolated Height at (60,60) = (2, 2): " + a.getInterpolatedHeight(new Point(60, 60)));
-		System.out.println("Interpolated Height at (60,123) = (2, 4.1): " + a.getInterpolatedHeight(new Point(60, 123)));
-		System.out.println("Interpolated Height at (123,60) = (4.1, 2): " + a.getInterpolatedHeight(new Point(123, 60)));
-		System.out.println("Interpolated Height at (123,123) = (4.1, 4.1): " + a.getInterpolatedHeight(new Point(123, 123)));
+	// FOR TESTING: REMOVE LATER
+	/*public static void main(String[] args) throws FileNotFoundException, IOException {
+		HeightMap a = new HeightMap("GC3.map");
+		a.saveBufferedImage(a.getBufferedImage(1000, 1000), "GC3");
 	}*/
 	
 	// default constructor
@@ -39,48 +37,59 @@ public class HeightMap{
 		readFile(filename);
 	}
 	
-	
 	// read in from file
 	public void readFile(String infile) throws IOException, FileNotFoundException{
-		try {
-			// read in the file (which is one line) and split on " "
-			BufferedReader input = new BufferedReader(new FileReader(infile));
-			String[] tokens = input.readLine().split(" ");
+		// read in the file (which is one line) and split on " "
+		BufferedReader input = new BufferedReader(new FileReader(infile));
+		String[] tokens = input.readLine().split(" ");
 		
-			// assign the first two values to the width & length
-			width = Integer.parseInt(tokens[0]);
-			length = Integer.parseInt(tokens[1]);
-			
-			// instantiate array
-			heightgrid = new double[width][length];
-			
-			// populate array
-			for (int w = 0; w<width; w++){
-				for (int l = 0; l<length; l++){
-					heightgrid[w][l] = Double.parseDouble(tokens[2+w+l*width]);
-				}
+		// assign the first two values to the width & length
+		width = Integer.parseInt(tokens[0]);
+		length = Integer.parseInt(tokens[1]);
+		
+		// instantiate array
+		heightgrid = new double[width][length];
+		
+		// populate array
+		for (int w = 0; w<width; w++){
+			for (int l = 0; l<length; l++){
+				heightgrid[w][l] = Double.parseDouble(tokens[2+w+l*width]);
+				
+				// check for high/low points
+				if (heightgrid[w][l] > highest)
+					highest = heightgrid[w][l];
+				if (heightgrid[w][l] < lowest)
+					lowest = heightgrid[w][l];
 			}
 		}
-		catch (FileNotFoundException e){
-			System.out.println("(zomg) OH NOES: " + e.getMessage());
-			throw(e);
-		}
-		catch (IOException e){
-			System.out.println("(zomg) OH NOES: " + e.getMessage());
-			throw(e);
-		}
+			
+		// set the range (to be used for colour gradient)
+		range = highest - lowest;
 	}
 		
 	// draw to buffered image
-	private BufferedImage drawBufferedImage(int w, int h){
-		BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
+	private BufferedImage drawBufferedImage(int w, int l){
+		xstep = w/((float)(width)-1);
+		ystep = l/((float)(length)-1);
+			
+		BufferedImage bi = new BufferedImage(w, l, BufferedImage.TYPE_BYTE_GRAY);
+		
 		// get a graphics2D object to draw onto the image
 		Graphics2D imageCreator = bi.createGraphics();
-		imageCreator.setBackground(Color.WHITE);
-		imageCreator.clearRect(0, 0, w, h);
-		imageCreator.setColor(Color.BLACK);
 		
-		// SOMEHOW?? fill the buffered image with the relevant colour based on interpolated height of the point
+		// loop through the pixels in the image
+		for (int x = 0; x < w; x++){
+			for (int y = 0; y < l; y++){
+				// colour in that pixel with the relevant height data
+				double currHeight = getInterpolatedHeight(new Point(x,y));
+				float heightPercent = (float)((currHeight-lowest)/range);
+				// @TODO: add colour gradient (maybe low saturation for visibility?)
+				imageCreator.setColor(new Color(heightPercent, heightPercent, heightPercent));
+				imageCreator.fillRect(l-y, w-x, 1, 1); // to get the correct rotation of the image
+			}
+		}
+		
+		//System.out.println("DONE with buffered image");
 		
 		return bi;
 	}
@@ -90,44 +99,50 @@ public class HeightMap{
 		return drawBufferedImage(w,h);
 	}
 	
+	// save buffered image -- takes in an image and a filename (NOTE: without extension)
+	public void saveBufferedImage(BufferedImage bi, String filename) throws FileNotFoundException, IOException{
+		File outputfile = new File(filename+".png");
+		ImageIO.write(bi, "png", outputfile);
+	}
+	
 	// get the height at a point with bilinear interpolation
 	public double getInterpolatedHeight(Point p){
 		// interpolated point
 		double ih;
-				
+		
 		// if the point is on a grid intersection
-		if (p.x % 30 == 0 && p.y % 30 == 0){
-			ih = heightgrid[p.x/30][p.y/30];
+		if (p.x % xstep == 0 && p.y % ystep == 0){
+			ih = heightgrid[(int)(p.x/xstep)][(int)(p.y/ystep)];
 		}
 		// if the point is on a vertical gridline
-		else if (p.x % 30 == 0){
-			int y1 = (int)Math.floor(p.y/30f);
-			int y2 = (int)Math.ceil(p.y/30f);
-			float y = p.y/30f;
+		else if (p.x % xstep == 0){
+			int y1 = (int)Math.floor(p.y/ystep);
+			int y2 = (int)Math.ceil(p.y/ystep);
+			float y = p.y/ystep;
 			
 			// INTERPOLATE over y
-			ih = ((y2-y)*heightgrid[p.x/30][y1]/(y2-y1)) + ((y-y1)*heightgrid[p.x/30][y2]/(y2-y1));
+			ih = ((y2-y)*heightgrid[(int)(p.x/xstep)][y1]/(y2-y1)) + ((y-y1)*heightgrid[(int)(p.x/xstep)][y2]/(y2-y1));
 		}
 		// if the point is on a horizontal gridline
-		else if (p.y % 30 == 0){
-			int x1 = (int)Math.floor(p.x/30f);
-			int x2 = (int)Math.ceil(p.x/30f);
-			float x = p.x/30f;
+		else if (p.y % ystep == 0){
+			int x1 = (int)Math.floor(p.x/xstep);
+			int x2 = (int)Math.ceil(p.x/xstep);
+			float x = p.x/xstep;
 			
 			// INTERPOLATE over x
-			ih = (((x2-x)*heightgrid[x1][p.y/30])/(x2-x1)) + (((x-x1)*heightgrid[x2][p.y/30])/(x2-x1));
+			ih = (((x2-x)*heightgrid[x1][(int)(p.y/ystep)])/(x2-x1)) + (((x-x1)*heightgrid[x2][(int)(p.y/ystep)])/(x2-x1));
 		}
 		// if the point is somewhere in a gridblock
 		else{
 			double xy1Height;
 			double xy2Height;
 			
-			int x1 = (int)Math.floor(p.x/30f);
-			int x2 = (int)Math.ceil(p.x/30f);
-			float x = p.x/30f;
-			int y1 = (int)Math.floor(p.y/30f);
-			int y2 = (int)Math.ceil(p.y/30f);
-			float y = p.y/30f;
+			int x1 = (int)Math.floor(p.x/xstep);
+			int x2 = (int)Math.ceil(p.x/xstep);
+			float x = p.x/xstep;
+			int y1 = (int)Math.floor(p.y/ystep);
+			int y2 = (int)Math.ceil(p.y/ystep);
+			float y = p.y/ystep;
 			
 			// INTERPOLATE over y1
 			xy1Height = ((x2-x)*heightgrid[x1][y1]/(x2-x1)) + ((x-x1)*heightgrid[x2][y1]/(x2-x1));
@@ -144,13 +159,11 @@ public class HeightMap{
 	public String toString(){
 		StringBuffer returnstr = new StringBuffer();
 		for (int i=0; i<width; i++){
-			System.out.println("got to row " + i);
 			for (int j=0; j<length; j++){
 				returnstr.append(heightgrid[i][j] + " ");
 			}
 			returnstr.append("\n");
-		}		
-		System.out.println("TOSTRING!");
+		}
 		return new String(returnstr);			
 	}
 }

3531956eb8cebc8551efe3e0eea10cb3911fc019
Bee Sharwood <bee.sharwood@gmail.com>
2012-09-30 10:56:57 +0200

    Separated heightmap functionality into Terrain and Heightmap
    -- Need to update usage of HeightMap elsewhere: Terrain should be used
    amongst the simulation; HeightMap is only for reading in the .map file
    and creating a Terrain (not sure where HeightMap has been used?)
    -- To follow: the calculation of gradient given a point and a direction
    -- To follow: serialisation?
    -- To follow: exporting terrain as a .obj file (but idk how to do this
    :<)

--- src/plainswalker/simulation/HeightMap.java
+++ src/plainswalker/simulation/HeightMap.java
@@ -4,112 +4,78 @@
 package plainswalker.simulation;
 
 import java.io.*;
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.image.BufferedImage;
 
-import javax.imageio.ImageIO;
 
 public class HeightMap{
 	// height grid: 2D array
-	double[][] heightgrid;
-	int length = 100, width = 100;
-	float xstep, ystep;
-	double lowest = Double.POSITIVE_INFINITY, highest = Double.NEGATIVE_INFINITY, range = 0;
-
-	// FOR TESTING: REMOVE LATER
-	/*public static void main(String[] args) throws FileNotFoundException, IOException {
-		HeightMap a = new HeightMap("GC3.map");
-		a.saveBufferedImage(a.getBufferedImage(1000, 1000), "GC3");
-	}*/
-
-	// default constructor
-	public HeightMap(){
-		heightgrid = new double[length][width];
-		for (int i=0; i<100; i++)
-			for (int j=0; j<100; j++)
-				heightgrid[i][j] = 0;
-	}
-
-	// constructor which takes in string filename: to be used more often
+	private double[][] heightgrid;
+	private int length = 100, width = 100;
+	private float xstep, ystep;
+	protected double lowest = Double.POSITIVE_INFINITY, highest = Double.NEGATIVE_INFINITY, range = 0;
+	
+	// constructor: takes in string filename of a text heightmap
 	public HeightMap(String filename) throws FileNotFoundException, IOException{
-		readFile(filename);
-	}
-
-	// read in from file
-	public void readFile(String infile) throws IOException, FileNotFoundException{
-		// read in the file (which is one line) and split on " "
-		BufferedReader input = new BufferedReader(new FileReader(infile));
-		String[] tokens = input.readLine().split(" ");
-
-		// assign the first two values to the width & length
-		width = Integer.parseInt(tokens[0]);
-		length = Integer.parseInt(tokens[1]);
-
-		// instantiate array
-		heightgrid = new double[width][length];
-
-		// populate array
-		for (int w = 0; w<width; w++){
-			for (int l = 0; l<length; l++){
-				heightgrid[w][l] = Double.parseDouble(tokens[2+w+l*width]);
-
-				// check for high/low points
-				if (heightgrid[w][l] > highest)
-					highest = heightgrid[w][l];
-				if (heightgrid[w][l] < lowest)
-					lowest = heightgrid[w][l];
+		try {
+			// read in the file (which is one line) and split on " "
+			BufferedReader input = new BufferedReader(new FileReader(filename));
+			String[] tokens = input.readLine().split(" ");
+		
+			// assign the first two values to the width & length
+			width = Integer.parseInt(tokens[0]);
+			length = Integer.parseInt(tokens[1]);
+			
+			// instantiate array
+			heightgrid = new double[width][length];
+			
+			// populate array
+			for (int w = 0; w<width; w++){
+				for (int l = 0; l<length; l++){
+					heightgrid[w][l] = Double.parseDouble(tokens[2+w+l*width]);
+					
+					// check for high/low points
+					if (heightgrid[w][l] > highest)
+						highest = heightgrid[w][l];
+					if (heightgrid[w][l] < lowest)
+						lowest = heightgrid[w][l];
+				}
 			}
+			
+			// set the range (to be used for colour gradient)
+			range = highest - lowest;
 		}
-
-		// set the range (to be used for colour gradient)
-		range = highest - lowest;
+		catch (FileNotFoundException e){
+			System.out.println("(zomg) OH NOES: " + e.getMessage());
+			throw(e);
+		}
+		catch (IOException e){
+			System.out.println("(zomg) OH NOES: " + e.getMessage());
+			throw(e);
+		}
 	}
-
-	// draw to buffered image
-	private BufferedImage drawBufferedImage(int w, int l){
+		
+	// create a more finally grained grid with linear interpolation
+	protected double[][] createTerrain(int w, int l){
 		xstep = w/((float)(width)-1);
 		ystep = l/((float)(length)-1);
-
-		BufferedImage bi = new BufferedImage(w, l, BufferedImage.TYPE_BYTE_GRAY);
-
-		// get a graphics2D object to draw onto the image
-		Graphics2D imageCreator = bi.createGraphics();
-
+			
+		double[][] terrain = new double[l][w];
+		
 		// loop through the pixels in the image
 		for (int x = 0; x < w; x++){
 			for (int y = 0; y < l; y++){
-				// colour in that pixel with the relevant height data
-				double currHeight = getInterpolatedHeight(new Point(x,y));
-				float heightPercent = (float)((currHeight-lowest)/range);
-				// @TODO: add colour gradient (maybe low saturation for visibility?)
-				imageCreator.setColor(new Color(heightPercent, heightPercent, heightPercent));
-				imageCreator.fillRect(l-y, w-x, 1, 1); // to get the correct rotation of the image
+				// to get the correct rotation of the image
+				terrain[l-y-1][w-x-1] = getInterpolatedHeight(new Point(x,y));
 			}
 		}
-
-		//System.out.println("DONE with buffered image");
-
-		return bi;
+		return terrain;
 	}
-
-	// get buffered image
-	public BufferedImage getBufferedImage(int w, int h){
-		return drawBufferedImage(w,h);
-	}
-
-	// save buffered image -- takes in an image and a filename (NOTE: without extension)
-	public void saveBufferedImage(BufferedImage bi, String filename) throws FileNotFoundException, IOException{
-		File outputfile = new File(filename+".png");
-		ImageIO.write(bi, "png", outputfile);
-	}
-
+	
 	// get the height at a point with bilinear interpolation
-	public double getInterpolatedHeight(Point p){
+	private double getInterpolatedHeight(Point p){
 		// interpolated point
 		double ih;
-
+		
 		// if the point is on a grid intersection
 		if (p.x % xstep == 0 && p.y % ystep == 0){
 			ih = heightgrid[(int)(p.x/xstep)][(int)(p.y/ystep)];
@@ -119,7 +85,7 @@ public class HeightMap{
 			int y1 = (int)Math.floor(p.y/ystep);
 			int y2 = (int)Math.ceil(p.y/ystep);
 			float y = p.y/ystep;
-
+			
 			// INTERPOLATE over y
 			ih = ((y2-y)*heightgrid[(int)(p.x/xstep)][y1]/(y2-y1)) + ((y-y1)*heightgrid[(int)(p.x/xstep)][y2]/(y2-y1));
 		}
@@ -128,7 +94,7 @@ public class HeightMap{
 			int x1 = (int)Math.floor(p.x/xstep);
 			int x2 = (int)Math.ceil(p.x/xstep);
 			float x = p.x/xstep;
-
+			
 			// INTERPOLATE over x
 			ih = (((x2-x)*heightgrid[x1][(int)(p.y/ystep)])/(x2-x1)) + (((x-x1)*heightgrid[x2][(int)(p.y/ystep)])/(x2-x1));
 		}
@@ -136,14 +102,14 @@ public class HeightMap{
 		else{
 			double xy1Height;
 			double xy2Height;
-
+			
 			int x1 = (int)Math.floor(p.x/xstep);
 			int x2 = (int)Math.ceil(p.x/xstep);
 			float x = p.x/xstep;
 			int y1 = (int)Math.floor(p.y/ystep);
 			int y2 = (int)Math.ceil(p.y/ystep);
 			float y = p.y/ystep;
-
+			
 			// INTERPOLATE over y1
 			xy1Height = ((x2-x)*heightgrid[x1][y1]/(x2-x1)) + ((x-x1)*heightgrid[x2][y1]/(x2-x1));
 			// INTERPOLATE over y2
@@ -151,25 +117,31 @@ public class HeightMap{
 			// INTERPOLATE over p.x
 			ih = ((y2-y)*xy1Height/(y2-y1)) + ((y-y1)*xy2Height/(y2-y1));
 		}
-
+		
 		return ih;
 	}
-
+	
+	// accessor for length
+	public int getLength(){
+		return length;
+	}
+	
+	// accessor for width
+	public int getWidth(){
+		return width;
+	}
+	
 	// print out the HeightMap
 	public String toString(){
 		StringBuffer returnstr = new StringBuffer();
 		for (int i=0; i<width; i++){
+			System.out.println("got to row " + i);
 			for (int j=0; j<length; j++){
 				returnstr.append(heightgrid[i][j] + " ");
 			}
 			returnstr.append("\n");
-		}
-		return new String(returnstr);
+		}		
+		System.out.println("TOSTRING!");
+		return new String(returnstr);			
 	}
-
-	//Getters for width and height
-
-	public int getWidth(){return width;}
-
-	public int getLength(){return length;}
 }
---
+++ src/plainswalker/simulation/Terrain.java
@@ -0,0 +1,110 @@
+// virtual representation of the world's terrain
+// constructed from a heightmap using bilinear interpolation
+
+package plainswalker.simulation;
+
+import java.io.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+public class Terrain{
+	HeightMap heights;
+	double[][] landscape;
+	int width, length;
+	double highest, lowest, range;
+	
+	// FOR TESTING: REMOVE LATER
+	/*public static void main(String[] args) throws FileNotFoundException, IOException {
+		Terrain t = new Terrain("GC3.map", 1000, 1000);
+		t.saveBufferedImage(t.getBufferedImage(), "GC3");
+	}*/
+	
+	// constructor -- takes in a heightmap, and a width and length
+	// the terrain will have as many unit steps as specified by length and width
+	public Terrain(HeightMap h, int w, int l) throws FileNotFoundException, IOException {
+		heights = h;
+		width = w;
+		length = l;
+		
+		landscape = heights.createTerrain(width, length);
+		
+		highest = heights.highest;
+		lowest = heights.lowest; 
+		range = heights.range;
+	}
+	
+	// constructor -- takes in a string filename (to construct a heightmap), and a width and length
+	// the terrain will have as many unit steps as specified by length and width
+	public Terrain(String filename, int w, int l) throws FileNotFoundException, IOException {
+		this(new HeightMap(filename),w,l);
+		heights = new HeightMap(filename);
+		width = w;
+		length = l;
+		
+		landscape = heights.createTerrain(width, length);
+				
+		highest = heights.highest;
+		lowest = heights.lowest; 
+		range = heights.range;
+	}
+	
+	// accessor for heights
+	public double getHeight(int x, int y){
+		return landscape[x][y];
+	}
+	
+	// draw the terrain to buffered image
+	private BufferedImage drawBufferedImage(){
+		BufferedImage bi = new BufferedImage(width, length, BufferedImage.TYPE_4BYTE_ABGR);
+		
+		// get a graphics2D object to draw onto the image
+		Graphics2D imageCreator = bi.createGraphics();
+		
+		// loop through the pixels in the image
+		for (int x = 0; x < width; x++){
+			for (int y = 0; y < length; y++){
+				// colour in that pixel with the relevant height data
+				double currHeight = landscape[x][y];
+				float heightPercent = (float)((currHeight-lowest)/range);
+				
+				// coloured heightmap using overflow of the RGB values
+				/*float red = 0, blue = 0; 	// initialize blue & green to zero 
+				float green = heightPercent;
+				if (green > 1){ 	// colour overflow spills into the next color
+					red = green-1; 
+					green = 1;
+					if (red > 1){
+						blue = red - 1;
+						red = 1;
+						if (blue > 1){
+							blue = 1;
+						}
+					}
+				}
+				imageCreator.setColor(new Color(red, green, blue));*/
+				
+				imageCreator.setColor(new Color(heightPercent, heightPercent, heightPercent));
+				imageCreator.fillRect(x, y, 1, 1);
+			}
+		}
+		
+		//System.out.println("DONE creating buffered image");
+		
+		return bi;
+	}
+	
+	// get buffered image - returns a graphical representation of the terrain
+	public BufferedImage getBufferedImage(){
+		return drawBufferedImage();
+	}
+	
+	// save buffered image -- takes in an image and a filename (NOTE: without extension)
+	public void saveBufferedImage(BufferedImage bi, String filename) throws FileNotFoundException, IOException{
+		File outputfile = new File(filename+".png");
+		ImageIO.write(bi, "png", outputfile);
+	}
+}
